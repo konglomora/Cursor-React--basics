2f228314eb2d98a109ee745b82819bd00a3316ac
Arsenii Netrebenko <boecrejha@gmail.com>
2021-07-29 23:02:49 +0300

    Create a React app and publish it to GitHub Pages

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React GH Pages
         </a>
       </header>
     </div>
